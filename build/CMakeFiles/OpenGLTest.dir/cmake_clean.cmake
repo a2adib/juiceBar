4ec62a57b4c71bd8c57696d0b5a4be10e145544d
a2adib <a3dib.life@gmail.com>
2026-04-29 00:02:49 +0600

    initial commit

---
+++ build/CMakeFiles/OpenGLTest.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLTest.dir/src/glad.c.o"
+  "CMakeFiles/OpenGLTest.dir/src/glad.c.o.d"
+  "CMakeFiles/OpenGLTest.dir/src/main.cpp.o"
+  "CMakeFiles/OpenGLTest.dir/src/main.cpp.o.d"
+  "OpenGLTest"
+  "OpenGLTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/OpenGLTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
